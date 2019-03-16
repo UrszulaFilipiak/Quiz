03d0b9c1d290613cc3d6f5b5e6a744c1c5cbfd18
Urszula Filipiak <urszula.filipiak.17@gmail.com>
2019-03-16 19:27:16 +0100

    Zmiana layoutu. Zmiana układu strony i klas (Widoczne 2 pytania na stronie, strzałki po lewej i prawej stronie pytań, dodanie transparentnego tła).

--- js/app.js
+++ js/app.js
@@ -1,3 +1,33 @@
 document.addEventListener("DOMContentLoaded", function (){
+    const questions = document.querySelector(".questions");
+    const question = document.querySelectorAll(".question");
+    const prev = document.querySelector(".previous-arrow");
+    const next = document.querySelector(".next-arrow");
+
+    const questionsWidth = question[0].clientWidth;
+    let currentIndex = 0;
+    let questionNumber = question.length - 1;
+
+    function goToQuestion(index) {
+
+        if (index <0) {
+            index = questionNumber;
+        } else if (index > questionNumber) {
+            index = 0;
+        }
+
+        question.style.left = index * (-questionsWidth) + "px";
+        currentIndex = index;
+    }
+
+    function questionToNext() {
+        goToQuestion(currentIndex + 1);
+    }
+
+    function questionToPrev() {
+        goToQuestion(currentIndex - 1);
+    }
+    prev.addEventListener("click", questionToPrev);
+    next.addEventListener("click", questionToNext);
 
 });
